29a4e35c4a3ac3e132da45b56ce2c84c3959ba73
OBouch21 <oliverbouchier@proton.me>
2024-02-18 17:02:57 +0000

    Update Flask app with form submission and view functionality

--- 03_Web_Application/app.py
+++ 03_Web_Application/app.py
@@ -1,5 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import engine
 app = Flask(__name__)
+
+def loadInvoicesFromDB():
+    with engine.connect() as connection:
+        result = connection.execute('SELECT * FROM invoices')
+        invoices = []
+        for row in result:
+            invoices.append(row)
+        return invoices
+
+def getFormResults():
+    print(request.form)
+    return [
+        request.form['invoiceNumber'],
+        request.form['custName'],
+        request.form['custAddress'],
+        request.form['invoiceDate'],
+        request.form['invoiceTotal'],
+        request.form['invoiceCurrency'],
+        request.form['invoiceDescription']
+    ]
 
 @app.route('/')
 def index():
@@ -9,9 +30,16 @@ def index():
 def create():
     return render_template('create.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    formResults = getFormResults()
+    with engine.connect() as connection:
+        connection.execute(f'INSERT INTO invoices (invoiceNumber, custName, custAddress, invoiceDate, invoiceTotal, invoiceCurrency, invoiceDescription) VALUES ({formResults[0]}, "{formResults[1]}", "{formResults[2]}", "{formResults[3]}", {formResults[4]}, "{formResults[5]}", "{formResults[6]}")')
+    return render_template('submit.html')
+
 @app.route('/view')
 def view():
-    return render_template('view.html')
+    return render_template('view.html', invoices=loadInvoicesFromDB())
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ 03_Web_Application/database.py
@@ -0,0 +1,21 @@
+import os
+# import pymysql
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+databaseUsername = os.getenv('DB_USERNAME')
+databasePassword = os.getenv('DB_PASS')
+
+dbConnectionString = f'mysql+pymysql://{databaseUsername}:{databasePassword}@aws.connect.psdb.cloud/oliverinvoices?charset=utf8mb4'
+
+# Create a connection to the database
+engine = create_engine(dbConnectionString,
+                       connect_args={'ssl': {
+                           "ssl_ca":"/etc/ssl/cert.pem"}})
+
+with engine.connect() as connection:
+    result = connection.execute('SELECT * FROM invoices')
+    for row in result:
+        print(type(row))
