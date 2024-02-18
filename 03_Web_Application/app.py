0e3356d933574cdcde988182551aff9e481949a4
OBouch21 <oliverbouchier@proton.me>
2024-02-18 13:42:10 +0000

    Add new routes for create and view pages

--- 03_Web_Application/app.py
+++ 03_Web_Application/app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
