6bddfab4f3f954c7a14ebb5b8aecc599a97911ad
OBouch21 <oliverbouchier@proton.me>
2024-02-19 10:21:40 +0000

    Update port

--- 03_Web_Application/app.py
+++ 03_Web_Application/app.py
@@ -82,4 +82,4 @@ def view():
     return render_template('view.html', invoices=loadInvoicesFromDB())
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=443)
+    app.run(host="0.0.0.0", port=5000)
